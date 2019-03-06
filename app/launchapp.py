485a90af5b8dc15eabb5267cf8437a14b69e6738
Andrew Stewart <andrew.stewart@elderresearch.com>
2019-03-06 18:28:38 -0500

    move data models to separate module

--- app/launchapp.py
+++ app/launchapp.py
@@ -18,20 +18,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+from .models import ActivityLog
 
-class ActivityLog(db.Model):
-    id = db.Column(db.String(64), primary_key=True)
-    username = db.Column(db.String(32))
-    image_type = db.Column(db.String(8))
-    num_gpus = db.Column(db.Integer)
-    start_time = db.Column(db.DateTime, default=datetime.utcnow)
-    stop_time = db.Column(db.DateTime, nullable=True)
-
-    def __repr__(self):
-        return "<ActivityLog {}".format(self.id)
-
-    def stop(self):
-        self.stop_time = datetime.utcnow()
 
 HISTORY = []
 LAUNCHED_SESSIONS = []
