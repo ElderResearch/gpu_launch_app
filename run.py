d12976c67e3afe153b4ec2fa2531dbf9a812264f
andrew.stewart <andrew.stewart@elderresearch.com>
2019-04-22 15:34:29 -0400

    add server runtime options for development purposes

--- run.py
+++ run.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
 
+import argparse
 from app import create_app
 
 server = create_app()
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    debug = "run server in debug mode"
+    parser.add_argument('-d', '--debug', action='store_true', help=debug)
+
+    port = "run server on specified port"
+    parser.add_argument('-p', '--port', default=5000, type=int, help=port)
+
+    return parser.parse_args()
+
 if __name__ == "__main__":
-    server.run('0.0.0.0')
+    args = parse_args()
+    server.run('0.0.0.0', port=args.port, debug=args.debug)
