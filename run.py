199b49aac4aa56407610b99ec9bdecbc3e60e38d
Andrew Stewart <andrew.stewart@elderresearch.com>
2019-04-30 20:59:27 -0400

    add shell context for testing and administration from a terminal session

--- run.py
+++ run.py
@@ -2,8 +2,14 @@
 
 import argparse
 from app import create_app
+from app.extensions import db
+from app.models import ActivityLog
 
 server = create_app()
+
+@server.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'ActivityLog': ActivityLog}
 
 def parse_args():
     parser = argparse.ArgumentParser()
