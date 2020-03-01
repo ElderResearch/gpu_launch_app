9646311c580b776780c0386ec6d64480cfab1132
Andrew Stewart <andrew.stewart@elderresearch.com>
2020-03-01 16:22:46 +0000

    update data generator for new db schema

--- app/helpers.py
+++ app/helpers.py
@@ -14,18 +14,19 @@ from app import db
 from app.models import ActivityLog, User
 
 
-def generate_usage_log_data(Model, n=100):
+def generate_usage_log_data(n=100):
     _names = ["andy", "betty", "bobby", "timmy", "sue"]
     _imagetypes = ["Python", "Python+R"]
     _numgpus = [0, 1, 2, 3, 4]
     containers = []
 
     for i in range(n):
+        user = User(username=random.choice(_names))
         td_start = timedelta(days=i % 30, hours=random.randint(0, 23))
         start = datetime.utcnow() - td_start
-        instance = Model(
+        instance = ActivityLog(
             id=secrets.token_hex(32),
-            username=random.choice(_names),
+            user=user,
             image_type=random.choice(_imagetypes),
             num_gpus=random.choice(_numgpus),
             start_time=start,
