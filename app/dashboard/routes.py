2ad4e2ab6bb33adf1c1751d095f622ef3d4c5554
Andrew Stewart <andrew.stewart@elderresearch.com>
2020-03-02 18:29:40 +0000

    wip: change container timeline to history table

--- app/dashboard/routes.py
+++ app/dashboard/routes.py
@@ -1,8 +1,9 @@
-from flask import render_template, request
+from flask import render_template, request, jsonify
 from flask_login import login_required
 
 from app.dashboard import bp
 from app.helpers import data_query
+from app.models import ActivityLog
 
 
 @bp.route("/", methods=["GET"])
@@ -17,3 +18,16 @@ def data():
     start_date = request.args.get("start_date")
     end_date = request.args.get("end_date")
     return data_query(start_date, end_date)
+
+
+@bp.route("/history", methods=["GET"])
+@login_required
+def history():
+    page = request.args.get("page")
+    per_page = request.args.get("per_page")
+    query = (
+        ActivityLog.query(ActivityLog)
+        .order_by(ActivityLog.start_time.desc())
+        .paginate(page=page, per_page=per_page)
+    )
+    return jsonify([item.serialize for item in query.items])
